b5f0cbc3f60e80d8f621dff225221f7b94e1af1b
Selam Beyene <SELAMBEYENE2@GMAIL.COM>
2024-11-20 02:11:06 +0000

    stubs up MessageDAO

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,71 @@
 package DAO;
 
+import Model.Message;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.List;
+
 public class MessageDAO {
-    
+
+    public Message createNewMessage(Message message) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
+    }
+
+    public List<Message> getAllMessages(Message message) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
+    }
+
+    public Message getMessageById(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
+    }
+
+    public Message deleteMessageById(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
+    }
+
+    public Message updateMessageById(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return null;
+    }
+
 }
