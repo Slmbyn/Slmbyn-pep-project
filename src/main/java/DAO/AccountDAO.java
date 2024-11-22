0d2644d856d4b7c302ef8c6f974e4d73e03ef0be
Selam Beyene <SELAMBEYENE2@GMAIL.COM>
2024-11-22 00:29:32 +0000

    implements AccountDao.registerNewUser

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -2,11 +2,40 @@ package DAO;
 
 import Model.Account;
 import Model.Message;
+import Util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
+
 
 public class AccountDAO {
     
     public Account registerNewUser(Account account) {
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "INSERT INTO Account (username, password) VALUES (?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+
+            ps.executeUpdate();
+
+            ResultSet pk = ps.getGeneratedKeys();
+
+            while(pk.next()){
+                int generated_account_id = pk.getInt(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,30 @@
 package Service;
+import java.util.List;
 
-// public class AccountService {
-//     private AccountDao authorDAO;
-// }
+import DAO.AccountDAO;
+import Model.Account;
+import Model.Message;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    public Account registerNewUser(Account account) {
+        return accountDAO.registerNewUser(account);
+    }
+
+    public Account loginUser(Account account) {
+        return accountDAO.loginUser(account);
+    }
+
+    public List<Message> getUsersMessages(Account account) {
+        return accountDAO.getUsersMessages(account);
+    }
+}
