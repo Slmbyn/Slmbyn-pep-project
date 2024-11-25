b87a9c908aa1503da0418df21d8041b7b24b56f4
Selam Beyene <SELAMBEYENE2@GMAIL.COM>
2024-11-25 22:38:34 +0000

    implements MessageService methods

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,39 @@
 package Service;
 
+import java.util.List;
+
+import DAO.MessageDAO;
+import Model.Message;
+
+
 public class MessageService {
-    
+    private MessageDAO messageDAO;
+
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
+
+    public Message createNewMessage(Message message){
+        return messageDAO.createNewMessage(message);
+    }
+
+    public List<Message> getAllMessages(){
+        return messageDAO.getAllMessages();
+    }
+
+    public Message getMessageById(int message_id) {
+        return messageDAO.getMessageById(message_id);
+    }
+
+    public Message deleteMessageById(int message_id){
+        return messageDAO.deleteMessageById(message_id);
+    }
+
+    public Message updateMessageById(Message message) {
+        return messageDAO.updateMessageById(message);
+    }
 }
